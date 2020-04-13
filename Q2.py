5a77b0618ff34ba1c79e45d17e08cc5203bd0aff
Alper Ünal <alper.unal92@hotmail.com>
2020-04-13 22:00:23 +0300

    Update Q2.py

--- Q2.py
+++ Q2.py
@@ -1,61 +1,73 @@
-MAX = 100; 
-  
-def luDecomposition(mat, n): 
-  
-    lower = [[0 for x in range(n)]  
-                for y in range(n)]; 
-    upper = [[0 for x in range(n)]  
-                for y in range(n)]; 
-                  
-    # Decomposing matrix into Upper  
-    # and Lower triangular matrix 
-    for i in range(n): 
-  
-        # Upper Triangular 
-        for k in range(i, n):  
-  
-            # Summation of L(i, j) * U(j, k) 
-            sum = 0; 
-            for j in range(i): 
-                sum += (lower[i][j] * upper[j][k]); 
-  
-            # Evaluating U(i, k) 
-            upper[i][k] = mat[i][k] - sum; 
-  
-        # Lower Triangular 
-        for k in range(i, n): 
-            if (i == k): 
-                lower[i][i] = 1; # Diagonal as 1 
-            else: 
-  
-                # Summation of L(k, j) * U(j, i) 
-                sum = 0; 
-                for j in range(i): 
-                    sum += (lower[k][j] * upper[j][i]); 
-  
-                # Evaluating L(k, i) 
-                lower[k][i] = int((mat[k][i] - sum) /
-                                       upper[i][i]); 
-  
-    # setw is for displaying nicely 
-    print("Lower Triangular\t\tUpper Triangular"); 
-  
-    # Displaying the result : 
-    for i in range(n): 
-          
-        # Lower 
-        for j in range(n): 
-            print(lower[i][j], end = "\t");  
-        print("", end = "\t"); 
-  
-        # Upper 
-        for j in range(n): 
-            print(upper[i][j], end = "\t"); 
-        print(""); 
-  
-# Driver code 
-mat = [[ 1,  1,  1], 
-       [ 4,  3, -1], 
-       [ 3,  5,  3]]; 
-  
-luDecomposition(mat, 3); 
+import pprint
+
+def mult_matrix(M, N):
+    """Multiply square matrices of same dimension M and N"""
+
+    # Converts N into a list of tuples of columns                                                                                                                                                                                                      
+    tuple_N = zip(*N)
+
+    # Nested list comprehension to calculate matrix multiplication                                                                                                                                                                                     
+    return [[sum(el_m * el_n for el_m, el_n in zip(row_m, col_n)) for col_n in tuple_N] for row_m in M]
+
+def pivot_matrix(M):
+    """Returns the pivoting matrix for M, used in Doolittle's method."""
+    m = len(M)
+
+    # Create an identity matrix, with floating point values                                                                                                                                                                                            
+    id_mat = [[float(i ==j) for i in xrange(m)] for j in xrange(m)]
+
+    # Rearrange the identity matrix such that the largest element of                                                                                                                                                                                   
+    # each column of M is placed on the diagonal of of M                                                                                                                                                                                               
+    for j in xrange(m):
+        row = max(xrange(j, m), key=lambda i: abs(M[i][j]))
+        if j != row:
+            # Swap the rows                                                                                                                                                                                                                            
+            id_mat[j], id_mat[row] = id_mat[row], id_mat[j]
+
+    return id_mat
+
+def lu_decomposition(A):
+    """Performs an LU Decomposition of A (which must be square)                                                                                                                                                                                        
+    into PA = LU. The function returns P, L and U."""
+    n = len(A)
+
+    # Create zero matrices for L and U                                                                                                                                                                                                                 
+    L = [[0.0] * n for i in xrange(n)]
+    U = [[0.0] * n for i in xrange(n)]
+
+    # Create the pivot matrix P and the multipled matrix PA                                                                                                                                                                                            
+    P = pivot_matrix(A)
+    PA = mult_matrix(P, A)
+
+    # Perform the LU Decomposition                                                                                                                                                                                                                     
+    for j in xrange(n):
+        # All diagonal entries of L are set to unity                                                                                                                                                                                                   
+        L[j][j] = 1.0
+
+        # LaTeX: u_{ij} = a_{ij} - \sum_{k=1}^{i-1} u_{kj} l_{ik}                                                                                                                                                                                      
+        for i in xrange(j+1):
+            s1 = sum(U[k][j] * L[i][k] for k in xrange(i))
+            U[i][j] = PA[i][j] - s1
+
+        # LaTeX: l_{ij} = \frac{1}{u_{jj}} (a_{ij} - \sum_{k=1}^{j-1} u_{kj} l_{ik} )                                                                                                                                                                  
+        for i in xrange(j, n):
+            s2 = sum(U[k][j] * L[i][k] for k in xrange(j))
+            L[i][j] = (PA[i][j] - s2) / U[j][j]
+
+    return (P, L, U)
+
+
+A = [[1, 1,  1], [4, 3, -1], [3, 5, 3]]
+P, L, U = lu_decomposition(A)
+
+print "A:"
+pprint.pprint(A)
+
+print "P:"
+pprint.pprint(P)
+
+print "L:"
+pprint.pprint(L)
+
+print "U:"
+pprint.pprint(U)
