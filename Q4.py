70e0179b58fe4cad8d68985185ff204c3f07dcee
Alper Ünal <alper.unal92@hotmail.com>
2020-04-13 22:17:38 +0300

    Update Q4.py

--- Q4.py
+++ Q4.py
@@ -1,51 +1,33 @@
-import math 
-MAX = 100; 
-  
-def Cholesky_Decomposition(matrix, n): 
-  
-    lower = [[0 for x in range(n + 1)]  
-                for y in range(n + 1)]; 
-  
-    # Decomposing a matrix 
-    # into Lower Triangular 
-    for i in range(n):  
-        for j in range(i + 1):  
-            sum1 = 0; 
-  
-            # sum1mation for diagnols 
-            if (j == i):  
-                for k in range(j): 
-                    sum1 += pow(lower[j][k], 2); 
-                lower[j][j] = int(math.sqrt(matrix[j][j] - sum1)); 
-            else: 
-                  
-                # Evaluating L(i, j) 
-                # using L(j, j) 
-                for k in range(j): 
-                    sum1 += (lower[i][k] *lower[j][k]); 
-                if(lower[j][j] > 0): 
-                    lower[i][j] = int((matrix[i][j] - sum1) / 
-                                               lower[j][j]); 
-  
-    # Displaying Lower Triangular 
-    # and its Transpose 
-    print("Lower Triangular\t\tTranspose"); 
-    for i in range(n):  
-          
-        # Lower Triangular 
-        for j in range(n): 
-            print(lower[i][j], end = "\t"); 
-        print("", end = "\t"); 
-          
-        # Transpose of 
-        # Lower Triangular 
-        for j in range(n): 
-            print(lower[j][i], end = "\t"); 
-        print(""); 
-  
-# Driver Code 
-n = 3; 
-matrix = [[ 2, -2, -3], 
-          [-2,  5,  4], 
-          [-3,  4,  5]]; 
-Cholesky_Decomposition(matrix, n);
+from math import sqrt
+from pprint import pprint
+ 
+def cholesky(A):
+    """Performs a Cholesky decomposition of A, which must 
+    be a symmetric and positive definite matrix. The function
+    returns the lower variant triangular matrix, L."""
+    n = len(A)
+
+    # Create zero matrix for L
+    L = [[0.0] * n for i in xrange(n)]
+
+    # Perform the Cholesky decomposition
+    for i in xrange(n):
+        for k in xrange(i+1):
+            tmp_sum = sum(L[i][j] * L[k][j] for j in xrange(k))
+            
+            if (i == k): # Diagonal elements
+                # LaTeX: l_{kk} = \sqrt{ a_{kk} - \sum^{k-1}_{j=1} l^2_{kj}}
+                L[i][k] = sqrt(A[i][i] - tmp_sum)
+            else:
+                # LaTeX: l_{ik} = \frac{1}{l_{kk}} \left( a_{ik} - \sum^{k-1}_{j=1} l_{ij} l_{kj} \right)
+                L[i][k] = (1.0 / L[k][k] * (A[i][k] - tmp_sum))
+    return L
+ 
+A = [[2, -2, -3], [-2, 5, 4], [-3, 4, 5]]
+L = cholesky(A)
+
+print "A:"
+pprint(A)
+
+print "L:"
+pprint(L)
