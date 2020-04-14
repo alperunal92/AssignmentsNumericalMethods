da9a5c760886929fb185f753d749ac9b34231da6
Alper Unal <alper.unal92@hotmail.com>
2020-04-14 22:01:09 +0300

    no message

--- Q3 Linear Equations Using Gauss Elimination with Scaled Row Pivoting.py
+++ Q3 Linear Equations Using Gauss Elimination with Scaled Row Pivoting.py
@@ -1,2 +1,50 @@
 # Maintainer Alper ÜNAL 1170393
 # Linear Equations Using Gauss Elimination with Scaled Row Pivoting
+
+## module gaussPivot
+’’’ x = gaussPivot(a,b,tol=1.0e-12).
+Solves [a]{x} = {b} by Gauss elimination with scaled row pivoting
+’’’
+import numpy as np
+import swap
+import error
+
+def gaussPivot(a,b,tol=1.0e-12):
+    n = len(b)
+    
+  # Set up scale factors
+    s = np.zeros(n)
+    for i in range(n):
+        s[i] = max(np.abs(a[i,:]))
+        
+    for k in range(0,n-1):
+    
+      # Row interchange, if needed
+        p = np.argmax(np.abs(a[k:n,k])/s[k:n]) + k
+    if abs(a[p,k]) < tol: error.err(’Matrix is singular’)
+    if p != k:
+        swap.swapRows(b,k,p)
+        swap.swapRows(s,k,p)
+        swap.swapRows(a,k,p)
+ # Elimination
+    for i in range(k+1,n):
+        if a[i,k] != 0.0:
+            lam = a[i,k]/a[k,k]
+            a[i,k+1:n] = a[i,k+1:n] - lam*a[k,k+1:n]
+            b[i] = b[i] - lam*b[k]
+    if abs(a[n-1,n-1]) < tol: error.err(’Matrix is singular’)
+ # Back substitution
+    b[n-1] = b[n-1]/a[n-1,n-1]
+    for k in range(n-2,-1,-1):
+        b[k] = (b[k] - np.dot(a[k,k+1:n],b[k+1:n]))/a[k,k]
+    return b
+
+ # Main program starts here
+    if __name__ == '__main__':
+        A = np.array([[1,  -1,   2,  1],
+                      [3,   2,   1,  4],
+                      [5,   8,   6,  3],
+                      [4,   2,   5,  3])
+        b = np.array([1, 1, 1, -1])
+        x = gauss(A, b)
+        print('Gauss result is x = \n %s' % x)
