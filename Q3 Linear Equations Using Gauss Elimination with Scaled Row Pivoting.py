01d25127c57975d62be14f9b980471f397c3178a
Alper Unal <alper.unal92@hotmail.com>
2020-04-14 22:23:29 +0300

    no message

--- Q3 Linear Equations Using Gauss Elimination with Scaled Row Pivoting.py
+++ Q3 Linear Equations Using Gauss Elimination with Scaled Row Pivoting.py
@@ -19,6 +19,7 @@ def gaussPivot(a,b,tol=1.0e-12):
         swap.swapRows(b,k,p)
         swap.swapRows(s,k,p)
         swap.swapRows(a,k,p)
+        
  # Elimination
     for i in range(k+1,n):
         if a[i,k] != 0.0:
@@ -26,6 +27,7 @@ def gaussPivot(a,b,tol=1.0e-12):
             a[i,k+1:n] = a[i,k+1:n] - lam*a[k,k+1:n]
             b[i] = b[i] - lam*b[k]
     if abs(a[n-1,n-1]) < tol: error.err(’Matrix is singular’)
+ 
  # Back substitution
     b[n-1] = b[n-1]/a[n-1,n-1]
     for k in range(n-2,-1,-1):
