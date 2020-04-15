7ccede7a1355610aa758f91ba8fc44b15aa66462
Alper Unal <alper.unal92@hotmail.com>
2020-04-15 13:35:57 +0300

    no message

--- Q3 Alper Unal 1170393.py
+++ Q3 Alper Unal 1170393.py
@@ -28,18 +28,18 @@ def gauss_pivot(a,b,tol=1.0e-12):
         for i in range(k+1,n):
             if a[i,k] != 0.0: # skip if a(i,k) is already zero
                 lam = a [i,k]/a[k,k] 
-                a[i,k+1:n] = a[i,k+1:n] - lam*a[k,k+1:n]
+                a[i,k:n] = a[i,k:n] - lam*a[k,k:n]
                 b[i] = b[i] - lam*b[k]
 
     if abs(a[n-1,n-1]) < tol: 
         raise Exception("Matrix is singular")
 
     # Back substitution phase, solution is substituted by b
-
-    b[n-1] = b[n-1]/a[n-1,n-1]
+    x = np.zeros_like(b)
+    x[n-1] = b[n-1]/a[n-1,n-1]
     for k in range(n-2,-1,-1):
-        b[k] = (b[k] - np.dot(a[k,k+1:n],b[k+1:n]))/a[k,k]
-    return b
+        x[k] = (b[k] - np.dot(a[k,k+1:n],x[k+1:n]))/a[k,k]
+    return x
 
     # Main program starts here
     if __name__ == '__main__':
