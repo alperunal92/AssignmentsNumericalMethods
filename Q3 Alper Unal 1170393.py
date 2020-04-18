4199302491e036bbaed521e9ac02e8764d7a576b
Alper Ünal <alper.unal92@hotmail.com>
2020-04-19 01:36:25 +0300

    Update Q3 Alper Unal 1170393.py

--- Q3 Alper Unal 1170393.py
+++ Q3 Alper Unal 1170393.py
@@ -1,44 +1,57 @@
-from numpy import *
-import swap    
-import error
+from numpy as np
 
-def gaussPivot(a,b,tol=1.0e-12):
-    n = len(b)
+def zamienW(M, i,j):
     
-  # Set up scale factors
-    s = zeros(n)
+    if len(M.shape) == 1:
+        M[i],M[j] = M[j],M[i]
+    else:
+        M[[i,j],:] = M[[j,i], :]
+
+def Gauss_scaled(A, B):
+    
+    n = len(A)
+    S = np.zeros(n)         
+    P = np.array(range(n))  
+    A= A.astype(np.float64)
+    B= B.astype(np.float64)
+
+    
     for i in range(n):
-        s[i] = max(abs(a[i,:]))
-            
-    for k in range(0,n-1):
-        
-      # Row interchange, if needed
-        p = int(argmax(abs(a[k:n,k])/s[k:n])) + k
-        if abs(a[p,k]) < tol: error.err('Matrix is singular')
-        if p != k:
-            swap.swapRows(b,k,p)
-            swap.swapRows(s,k,p)
-            swap.swapRows(a,k,p)
-            
-      # Elimination
-        for i in range(k+1,n):
-            if a[i,k] != 0.0:
-                lam = a[i,k]/a[k,k]
-                a[i,k+1:n] = a [i,k+1:n] - lam*a[k,k+1:n]
-                b[i] = b[i] - lam*b[k]
-    if abs(a[n-1,n-1]) < tol: error.err('Matrix is singular')
-                   
-  # Back substitution
-    for k in range(n-1,-1,-1):
-        b[k] = (b[k] - dot(a[k,k+1:n],b[k+1:n]))/a[k,k]
-    return b
+        S[i]= max(abs(A[i,:]))
+
+    for k in range(0, n-1):
+       
+        for j in range(k+1, n):
+            if np.abs(A[j][k])/S[j] >= abs(A[i][k])/S[i]:
+                pivot =j
+
+        if pivot != k:
+            zamienW(B,k,pivot)
+            zamienW(S,k,pivot)
+            zamienW(A,k,pivot)
+
+        for i in range(k+1, n):
+            A[k][k] = 0.0 if np.isclose(A[k][k], 0) else A[k][k]
+            z = A[i][k] / A[k][k]
+            A[i][k] = z
+
+            for j in range(k+1, n):
+                A[i][j] = A[i][j] - z* A[k][j]
+            B[i] = B[i] - z*B[k]
+
+    B[n-1] = B[n-1]/A[n-1,n-1]
+    for k in range(n-2,-1,-1):
+        A[k][k] = 0.0 if np.isclose(A[k][k], 0) else A[k][k]
+        B[k] = (B[k] - np.dot(A[k,k+1:n],B[k+1:n]))/A[k,k]
+
+    return B
 
     # Main program starts here
     if __name__ == '__main__':
-        a = np.array([[1,  -1,   2,  1],
+        A = np.array([[1,  -1,   2,  1],
                       [3,   2,   1,  4],
                       [5,   8,   6,  3],
                       [4,   2,   5,  3]])
-        b = np.array([1, 1, 1, -1])
-        x = gaussPivot(a, b)
+        B = np.array([1, 1, 1, -1])
+        x = gaussPivot(A, B)
         print('Gauss result is x = \n %s' % x)
