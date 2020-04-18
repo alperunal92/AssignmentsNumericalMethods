af57f2920e595c3d103da9a35aef8cfb4195c8eb
Alper Ünal <alper.unal92@hotmail.com>
2020-04-19 00:58:07 +0300

    Update Q3 Alper Unal 1170393.py

--- Q3 Alper Unal 1170393.py
+++ Q3 Alper Unal 1170393.py
@@ -1,44 +1,41 @@
-import numpy as np
+from numpy import zeros,argmax,dot
+from modules import swap
 
-def gauss_pivot(a,b,tol=1.0e-12):
-    """
-    x = gaussPivot(a,b,tol=1.0e-12).
+def gaussPivot(a,b,tol=1.0e-12):
+    n = len(b)
+
+''' ## module gaussPivot
+    x = gaussPivot(a,b,tol=1.0e-9).
     Solves [a]{x} = {b} by Gauss elimination with
     scaled row pivoting
-    """
-    a = np.copy(a)
-    b = np.copy(b)
-    n = len(b)
-    assert (np.all(np.shape(a) ==(n,n))) # check if a is a square matrix
-    # Set up scale factors
-    s = np.zeros(n)
+'''
+  # Set up scale factors
+    s = zeros(n)
     for i in range(n):
-        s[i] = max(np.abs(a[i,:])) # find the max of each row
-    for k in range(0, n-1): #pivot row
-        # Row interchange, if needed
-        p = np.argmax(np.abs(a[k:n,k])/s[k:n]) # find which row has max item for each col k, and scale by s
-        if abs(a[p,k]) < tol: 
-            raise Exception("Matrix is singular")
-        if p != k: # swap rows if current row does not contain max item with the one contains max item within same col
-            a[[k,p+k],:] = a[[p+k, k],:]
-            b[k],b[p+k] = b[p+k],b[k] 
-            s[k],s[p+k] = s[p+k],s[k]
+        s[i] = max(abs(a[i,:]))
 
-        # Elimination phase of matrix a
+    for k in range(0,n-1):
+
+      # Row interchange, if needed
+        p = argmax(abs(a[k:n,k])/s[k:n]) + k
+        if abs(a[p,k]) < tol: print('Matrix is singular')
+        if p != k:
+            swap.swapRows(b,k,p)
+            swap.swapRows(s,k,p)
+            swap.swapRows(a,k,p)
+
+      # Elimination
         for i in range(k+1,n):
-            if a[i,k] != 0.0: # skip if a(i,k) is already zero
-                lam = a [i,k]/a[k,k] 
-                a[i,k+1:n] = a[i,k+1:n] - lam*a[k,k+1:n]
+            if a[i,k] != 0.0:
+                lam = a[i,k]/a[k,k]
+                a[i,k+1:n] = a [i,k+1:n] - lam*a[k,k+1:n]
                 b[i] = b[i] - lam*b[k]
+    if abs(a[n-1,n-1]) < tol: print('Matrix is singular')
 
-    if abs(a[n-1,n-1]) < tol: 
-        raise Exception("Matrix is singular")
-
-    # Back substitution phase, solution is substituted by b
-
+  # Back substitution
     b[n-1] = b[n-1]/a[n-1,n-1]
     for k in range(n-2,-1,-1):
-        b[k] = (b[k] - np.dot(a[k,k+1:n],b[k+1:n]))/a[k,k]
+        b[k] = (b[k] - dot(a[k,k+1:n],b[k+1:n]))/a[k,k]
     return b
 
     # Main program starts here
