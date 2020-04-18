58e261f3cf8a6da5419825dcf8291a615c35fbd1
Alper Ünal <alper.unal92@hotmail.com>
2020-04-19 01:51:43 +0300

    Update Q3 Alper Unal 1170393.py

--- Q3 Alper Unal 1170393.py
+++ Q3 Alper Unal 1170393.py
@@ -1,57 +1,36 @@
 import numpy as np
 
-def zamienW(M, i,j):
-    
-    if len(M.shape) == 1:
-        M[i],M[j] = M[j],M[i]
-    else:
-        M[[i,j],:] = M[[j,i], :]
+A = np.array([[1,  -1,   2,  1],
+              [3,   2,   1,  4],
+              [5,   8,   6,  3],
+              [4,   2,   5,  3]])
+R = np.array([1, 1, 1, -1])
+AR=np.append(A,R,axis=1)  #append the right-handside vector into coefficient matrix as a column
+n=len(AR)      #size of the coefficient array (number of rows)
 
-def Gauss_scaled(A, B):
-    
-    n = len(A)
-    S = np.zeros(n)         
-    P = np.array(range(n))  
-    A= A.astype(np.float64)
-    B= B.astype(np.float64)
+for k in range(0,n):
+    pivot=abs(AR[k,k])
+    p_row=k
+    for i in range(k+1,n):  #find the largest element in pivot column
+        if abs(AR[i,k])>pivot:
+            p_row=i
+            pivot=abs(AR[i,k])
+    if p_row>k:
+        for j in range (0,n+1):  #exchange row with biggest element with the pivot row
+            temp=AR[k,j]
+            AR[k,j]=AR[p_row,j]
+            AR[p_row, j]=temp
+    for i in range(k+1,n):  #Use pivot row to eliminate elements below main diagonal
+        s=-AR[i,k]/AR[k,k]
+        for j in range(k,n+1):
+            AR[i,j]=AR[i,j]+s*AR[k,j]
 
-    
-    for i in range(n):
-        S[i]= max(abs(A[i,:]))
+X=np.zeros([n,1])
+X[n-1]=AR[n-1,n]/AR[n-1,n-1]
 
-    for k in range(0, n-1):
-       
-        for j in range(k+1, n):
-            if np.abs(A[j][k])/S[j] >= abs(A[i][k])/S[i]:
-                pivot =j
-
-        if pivot != k:
-            zamienW(B,k,pivot)
-            zamienW(S,k,pivot)
-            zamienW(A,k,pivot)
-
-        for i in range(k+1, n):
-            A[k][k] = 0.0 if np.isclose(A[k][k], 0) else A[k][k]
-            z = A[i][k] / A[k][k]
-            A[i][k] = z
-
-            for j in range(k+1, n):
-                A[i][j] = A[i][j] - z* A[k][j]
-            B[i] = B[i] - z*B[k]
-
-    B[n-1] = B[n-1]/A[n-1,n-1]
-    for k in range(n-2,-1,-1):
-        A[k][k] = 0.0 if np.isclose(A[k][k], 0) else A[k][k]
-        B[k] = (B[k] - np.dot(A[k,k+1:n],B[k+1:n]))/A[k,k]
-
-    return B
-
-    # Main program starts here
-    if __name__ == '__main__':
-        A = np.array([[1,  -1,   2,  1],
-                      [3,   2,   1,  4],
-                      [5,   8,   6,  3],
-                      [4,   2,   5,  3]])
-        B = np.array([1, 1, 1, -1])
-        x = Gauss_scaled(A, B)
-        print('Gauss result is x = \n %s' % x)
+for i in range(n-1,-1,-1):
+    sum=0
+    for j in range(i+1,n):
+        sum=sum+AR[i,j]*X[j]
+    X[i]=(AR[i,n]-sum)/AR[i,i]
+print(X)
